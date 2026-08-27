bf306f15d289476a294070ba614a00ae019ff412
Ahmed Fathy <ahmedgfathy@gmail.com>
2026-08-27 18:18:07 +0300

    Fix build: fix import, move images-source outside project

--- src/components/ProjectsPage.tsx
+++ src/components/ProjectsPage.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { projects, projectCategories } from '../data/projects'
+import { projects, categories as projectCategories } from '../data/projects'
 import { useState } from 'react'
 
 const ProjectsPage = () => {
